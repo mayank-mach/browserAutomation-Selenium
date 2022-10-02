313b5908cdde39cd3842042bf379a8546505dd66
Mayank Joshi <mayank.mach@hotmail.com>
2022-10-02 22:27:34 +0530

    Add function to launch a webpage

---
+++ Public/Open-WebPage.ps1
@@ -0,0 +1,31 @@
+﻿function Open-WebPage
+{
+    [CmdletBinding()]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   Position=0        
+        )]
+        [uri]$URL,
+
+        # Param2 help description
+        [Parameter(Mandatory=$true,
+                   Position=1        
+        )]
+        $Target
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        Write-Verbose "launching webpage $URL in selenium managed $($Target.Capabilities.BrowserName) browser"
+
+        $Target.Navigate().GoToUrl($URL) 
+    }
+    End
+    {
+    }
+}
